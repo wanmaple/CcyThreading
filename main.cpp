c47ee5ea55be597ec945087b415397407cdcbb81
Winder <wan_maple@hotmail.com>
2020-08-13 17:24:25 +0800

    new notes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <future>
 #include "src/thread_guard.h"
 #include "src/scoped_thread.h"
 #include "src/threadsafe_queue.h"
@@ -8,20 +9,7 @@
 using namespace std;
 using namespace ccy;
 
-class A
-{
-public:
-	int i;
-};
-
-void doSth(int i)
-{
-	cout << i << endl;
-}
-
 int main()
 {
-	threadsafe_queue<int> queue;
-
 	return 0;
 }
